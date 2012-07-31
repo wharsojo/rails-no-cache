6791daa5af559721080da067c188abb210acdab5
Widi Harsojo <wharsojo@Widis-iMac.local>
2012-07-31 10:56:25 +0700

    update cache header

--- lib/rails-no-cache.rb
+++ lib/rails-no-cache.rb
@@ -9,6 +9,8 @@ module ActionController
     def render(*args) #:nodoc:
       render_original_no_cache(*args)
       self.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      self.headers["Pragma"] = "no-cache"
+      self.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
       response_body
     end
   end
